116714998dad5ec2d942ef7eec8cbd809451b2cd
tieshou <tieshou@baofu.com>
2018-01-08 17:18:17 +0800

    删除

--- global-account-client-common/src/main/java/com/baofu/international/global/account/client/common/enums/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by BF100100 on 2017/11/3.
- */
-package com.baofu.international.global.account.client.common.enums;
--- global-account-client-web/src/main/webapp/login.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-
-<h2>this is login page!</h2>
-</body>
-</html>
